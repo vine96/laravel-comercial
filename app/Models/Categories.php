245d501057804d9be10b2b5c6b3246647f10b428
Vinícius Furtado Pecci de Lima <viniciuspecci@hotmail.com>
2022-02-13 18:25:58 -0300

    Criação da model, migration, seed e factory de Categories

---
+++ app/Models/Categories.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Categories extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name', 'img'
+    ];
+
+    protected $dates= [
+        'deleted_at'
+    ];
+}
